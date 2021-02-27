13fc06351793340d39cc0a71a19ed22a7097b87f
= <grzegorzszczesny794@gmail.com>
2021-02-27 14:09:39 +0100

    Fix duplicate Layout bug.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './styles/index.css'
 import CookieConsent from 'react-cookie-consent'
 
-import Layout from './layout/Layout'
-
 import {HashRouter  as Router, Route} from 'react-router-dom'
 import Home from './layout/MainPage'
 import Posts from './Post/Posts'
@@ -11,10 +9,8 @@ const App = () => {
   return(
    <div>
       <Router>
-     <Layout>
       <Route component={Home} exact path="/" />
       <Route component={Posts} exact path="/posts" />
-      </Layout>
     </Router>
     <CookieConsent
       overlay
