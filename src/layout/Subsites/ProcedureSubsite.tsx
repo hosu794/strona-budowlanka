e9eb98291be9705d9c21f764d1b7446809a19cc9
hosu794 <grzegorzszczesny794@gmail.com>
2021-06-22 18:04:03 +0200

    Add redirect to external webiste from subsite.

--- src/layout/Subsites/ProcedureSubsite.tsx
+++ src/layout/Subsites/ProcedureSubsite.tsx
@@ -16,6 +16,12 @@ const ProcedureSubsite = () => {
     axios
       .get(`${API_SERVER}wp-json/api/v1/procedures/subsite/${params.id}`)
       .then((response) => {
+        const content: string = response.data[0].post_content;
+
+        if (content.substring(0, 4) === "http") {
+          window.location.href = content;
+        }
+
         return setSubsite(response.data[0]);
       });
   }, [params]);
