641bc86c7ddab34aa9f4a108693eedbb9176e955
hosu794 <grzegorzszczesny794@gmail.com>
2021-07-23 20:19:42 +0200

    Add some fixies.

--- src/Post/PostPage.tsx
+++ src/Post/PostPage.tsx
@@ -15,22 +15,3 @@ const PostPage: React.FC<any> = ({ id, content, title, date }) => {
 };
 
 export default PostPage;
-
-type ShowMoreProps = {
-  length?: number;
-  content: string;
-  showMore: boolean;
-};
-
-const ShowMore = ({ showMore, length = 300, content }: ShowMoreProps) => {
-  return (
-    <React.Fragment>
-      <div
-        className="flex items-center flex-col break-all"
-        dangerouslySetInnerHTML={{
-          __html: showMore ? content : `${content.slice(0, length)}...`,
-        }}
-      ></div>
-    </React.Fragment>
-  );
-};
